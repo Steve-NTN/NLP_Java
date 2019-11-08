64a4eca0f5821e7c98e7b49f442ff586604a75a0
anhnghia47 <sam@google.com>
2019-11-09 03:00:20 +0700

    Read file txt and test

--- src/Input.java
+++ src/Input.java
@@ -1,8 +1,11 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Input {
+    List<String> listWord = new ArrayList<String>();
     public void input(String namefile) throws IOException {
         File file = new File(namefile);
         if (!file.exists()) {
@@ -12,10 +15,8 @@ public class Input {
         Scanner sc = new Scanner(file);
         while (sc.hasNextLine()){
             String str = sc.nextLine();
-            System.out.println(str);
+            listWord.add(str);
         }
-
-
 
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,30 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main
 {
+
+
     public static void main(String[] args) throws IOException {
-        ISpellingCorrector sc = new SpellingCorrector(10000);
+        ISpellingCorrector sc = new SpellingCorrector(100000);
 
         //Read data from file
-        //Input i = new Input();
-        //i.input("words.txt");
+        Input inp = new Input();
+        inp.input("words.txt");
 
-        //Test with word "wrong"
-        sc.putWord("wrong");
-        System.out.println(sc.correct("worng")); // Will return wrong
-        System.out.println(sc.correct("wroing")); // Will return wrong
+        //Put all words into list
+        for (int i = 0; i < inp.listWord.size() - 1; i++){
+            sc.putWord(inp.listWord.get(i));
+        }
+
+        //Press a word to test
+        Scanner s = new Scanner(System.in);
+        System.out.println("Press a word to test:");
+        String word = s.nextLine();
+
+        System.out.println(sc.correct(word));
 
     }
 }
