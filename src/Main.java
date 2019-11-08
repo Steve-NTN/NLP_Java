d42065df7927174dc1133ea926d7dca2ddab0194
anhnghia47 <sam@google.com>
2019-11-09 00:08:37 +0700

    Edit file Main.java to test word 'wrong'

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,14 @@ public class Main
     public static void main(String[] args) throws IOException {
         ISpellingCorrector sc = new SpellingCorrector(10000);
 
+        //Read data from file
+        //Input i = new Input();
+        //i.input("words.txt");
 
-
-        Input i = new Input();
-
-        i.input("words.txt");
-
-
-        //System.out.print(sc.correct("hellou"));
+        //Test with word "wrong"
+        sc.putWord("wrong");
+        System.out.println(sc.correct("worng")); // Will return wrong
+        System.out.println(sc.correct("wroing")); // Will return wrong
 
     }
 }
